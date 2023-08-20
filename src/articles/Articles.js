8d345d7d3558c5ec18a6df233c3f42b7f5ed43cd
Nadav-Zohar <nadavzohar542@gmail.com>
2023-08-20 23:23:51 +0300

    added

--- src/articles/Articles.js
+++ src/articles/Articles.js
@@ -1,7 +1,70 @@
+import { useEffect, useState } from "react";
 import "./Articles.css";
+import moment from 'moment';
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+
 
 export default function Articles(){
+    const [articles, setArticles]= useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.shipap.co.il/articles`, {
+            credentials: 'include',
+        })
+        .then(res => res.json())
+        .then(data => {
+            setArticles(data);
+            console.log(data);
+        });
+    }, []);
+
+    function removeArticle(id) {
+        if (!window.confirm("are you sure?")) {
+            return;
+        }
+
+        fetch(`https://api.shipap.co.il/articles/:id`, {
+            credentials: 'include',
+            method: 'DELETE',
+        })
+        .then(() => {
+            setArticles(articles.filter(x => x.id !== id));
+        });
+    }
+
+
+    
     return(
-        <p>articles managment</p>
+        <>
+<table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Headline</th>
+                        <th>Added Time</th>
+                        <th>Publish Date</th>
+                        <th>Views</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        articles.map((a, i) => 
+                            <tr key={a.id}>
+                                <td>{i + 1}</td>
+                                <td>{a.headline}</td>
+                                <td>{moment(a.addedTime).format('DD/MM/YY')}</td>
+                                <td>{moment(a.publishDate).format('DD/MM')}</td>
+                                <td>{a.views}</td>
+                                <td>
+                                    <button className="edit"><AiFillEdit /></button>
+                                    <button className="remove" onClick={() => removeArticle(a.id)}><AiFillDelete /></button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
